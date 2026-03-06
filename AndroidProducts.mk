2ee2652e2ab33f9d8133ba6da84ba56d4d20809b
YFMARCO-Dev <qahralandrwyd4@gmail.com>
2026-03-06 02:36:50 +0200

    Update AndroidProducts.mk and BoardConfig.mk for TWRP configuration; add mkbootimg.mk for boot and recovery image handling

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -8,7 +8,4 @@
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/twrp_a34x.mk
 
-COMMON_LUNCH_CHOICES := \
-    twrp_a34x-user \
-    twrp_a34x-userdebug \
-    twrp_a34x-eng
+COMMON_LUNCH_CHOICES := twrp_a34x-ap2a-eng
