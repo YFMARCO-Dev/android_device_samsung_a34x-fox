8b5607488e5fd72eeb918eca5dbfbf36c8bef874
YFMARCO-Dev <qahralandrwyd4@gmail.com>
2026-03-06 02:47:17 +0200

    twrp flags

--- device.mk
+++ device.mk
@@ -6,3 +6,21 @@
 #
 
 LOCAL_PATH := device/samsung/a34x
+
+# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
+# Health
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-impl.recovery \
+    android.hardware.health@2.1-service
+
+# fastbootd
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.0-impl-mock \
+    fastbootd
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
